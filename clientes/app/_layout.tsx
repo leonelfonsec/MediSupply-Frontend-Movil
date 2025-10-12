d4fc6c751b4afd76ea12588398b18e55df252534
Julianrvillamil <julianrvillamil@gmail.com>
2025-10-12 11:19:00 -0500

    feat: agregar soporte para @tanstack/react-query, axios y zustand en el proyecto

--- clientes/app/_layout.tsx
+++ clientes/app/_layout.tsx
@@ -3,9 +3,12 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import "../global.css"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import "../global.css";
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useState } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -14,13 +17,30 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+    // Crear una instancia única del QueryClient
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5, // 5 minutos
+        gcTime: 1000 * 60 * 10, // 10 minutos (antes cacheTime)
+        retry: 3,
+        refetchOnWindowFocus: false,
+      },
+      mutations: {
+        retry: 1,
+      },
+    },
+  }));
+  
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
